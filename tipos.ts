a2a2324e6cad2e32695fde58fe2edf4be01fae91
William Kiyoshi Taniguchi <wilkiyoshi>
2021-07-17 22:48:00 -0300

    atualização de arquivos

--- tipos.ts
+++ tipos.ts
@@ -45,3 +45,18 @@ function criar(objecto:object){
     //
 }
 criar ({propriedade:1})
+
+//never
+function nuncaOcorre():never{
+    while (true);
+}
+
+function erro(mensagem:string):never{
+    throw new Error(mensagem)
+}
+
+function falha(){
+    return erro('algo falhou')
+}
+
+//
