7f3148aa13d103fbcb19c5ead1d7d9b8b485856f
William Kiyoshi Taniguchi <wilkiyoshi>
2021-07-17 22:41:43 -0300

    atualização de arquivos

---
+++ tipos.ts
@@ -0,0 +1,43 @@
+//bolean
+const contaPaga:boolean = false;
+
+//number
+const idade:number = 23;
+const avaliacao:number = 4.5;
+
+//string
+const nome:string = "Will"
+
+//array
+const idades:number[] = [10,13,30,45,60];
+const nomes:string[] = ["João","Will","Leo","Fulano"]
+const idades2:Array<number> = [1,2,3,4,5]
+
+//tuple
+let jogadores:[string,string,string];
+jogadores = ["Will","JOão","Ciclano"];
+
+let jogadores2:[string,boolean,number];
+jogadores2 = ["Will", false,2];
+
+//enum
+enum StatusAprovado{
+    Aprovado="01",
+    Pendente="02",
+    Rejeitado="03"
+}
+const aprovacao: StatusAprovado = StatusAprovado.Aprovado
+
+//any
+const retornoAPI: any[]= [1,2,3,"Vitor",false];
+
+//void
+function printar(mensagem:string):void{
+    console.log(mensagem)
+}
+
+//null e undefined
+const u:undefined = undefined;
+const n:null = null;
+
+//object
